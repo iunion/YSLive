a0c9a83f3a55e9ee0dd201ccac2f59ec43e0a56b
DennisDeng <iunion@live.cn>
2020-02-06 19:47:53 +0800

    添加课程类别和详情文件

---
+++ YSOnlineSchool/Class/Cell/YSClassMediumCell.h
@@ -0,0 +1,17 @@
+//
+//  YSClassMediumCell.h
+//  YSAll
+//
+//  Created by jiang deng on 2020/2/6.
+//  Copyright © 2020 YS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YSClassMediumCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YSOnlineSchool/Class/YSClassDetailVC.m
@@ -0,0 +1,32 @@
+//
+//  YSClassDetailVC.m
+//  YSAll
+//
+//  Created by jiang deng on 2020/2/6.
+//  Copyright © 2020 YS. All rights reserved.
+//
+
+#import "YSClassDetailVC.h"
+
+@interface YSClassDetailVC ()
+
+@end
+
+@implementation YSClassDetailVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
