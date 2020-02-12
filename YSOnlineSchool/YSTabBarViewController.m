903162b31d2c708269cfe3d3219e4af99594be98
DennisDeng <iunion@live.cn>
2020-02-12 15:49:55 +0800

    禁止网校页面屏幕旋转

--- YSOnlineSchool/YSTabBarViewController.m
+++ YSOnlineSchool/YSTabBarViewController.m
@@ -22,6 +22,28 @@
 
 @implementation YSTabBarViewController
 
+#pragma mark 横竖屏
+
+/// 1.决定当前界面是否开启自动转屏，如果返回NO，后面两个方法也不会被调用，只是会支持默认的方向
+- (BOOL)shouldAutorotate
+{
+    return NO;
+}
+
+/// 2.返回支持的旋转方向
+/// iPad设备上，默认返回值UIInterfaceOrientationMaskAllButUpSideDwon
+/// iPad设备上，默认返回值是UIInterfaceOrientationMaskAll
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskPortrait;
+}
+
+/// 3.返回进入界面默认显示方向
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
+{
+    return UIInterfaceOrientationPortrait;
+}
+
 // 初始化所有Item
 - (instancetype)initWithDefaultItems
 {
