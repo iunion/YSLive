7b1f01831a3c7a24dcf3383c8b9bb0855e23b7c5
DennisDeng <iunion@live.cn>
2020-03-01 18:27:15 +0800

    joinroom  H5跳转app 使用除老师和学生身份外全部不做处理

--- YSLive/Login/YSLoginVC.m
+++ YSLive/Login/YSLoginVC.m
@@ -249,7 +249,10 @@
 #if 0
 //    NSString *urlstr = @"joinroom://rddoccdndemows.roadofcloud.com/static/h5_live_2.1.1.16/index.html/?host=release.roadofcloud.com&domain=xzj&param=oQWJiPESSSloUJYW_eebY4yhaXjcSeaZpBOt-tb2Cin88FjhbovGoYEX4dwrhvbuqYDqikDGwcB2bh3nMEiDhD7Vf-GmIxIs_tB_CdQZIiQrcC3ZIkUOS6NH9ks6LYfKu33bWttb7llfvnUU8_0C3A&timestamp=1581314212&roomtype=3&logintype=2&video=320*180&companyidentify=1";
         //NSString *urlstr = @"joinroom://?host=api.roadofcloud.net&domain=wjy&param=JxMe2Nu5uY9Bb5C_hStqSGuavpYFRNVVeHLFDFPH-R_q7cduxOZzR4i7XX3TqgytZtMeGuLhBSaXK4Gw6IXs7YZZQLFGu5SyULxpCxSfIJ6vuff28NGkwAq19EcpO7lBOAbgZ6Iv5XgJs26-2lNy4pZxaiTiGVbXAre7LrqaoVk&timestamp=1581327248&roomtype=3&logintype=2&video=200*150&companyidentify=1";
-    NSString *urlstr = @"joinroom://?host=api.roadofcloud.net&domain=wjy&param=JxMe2Nu5uY-l_bzNjinmoaeL6LbNaatpEnJM0sSUj6In0bo9pmxZMFqVdhpay2ki8fgtSO-azH9m0x4a4uEFJpcrgbDoheztZn7cF4vFUetQvGkLFJ8gnq-59_bw0aTACrX0Ryk7uUE4BuBnoi_leAmzbr7aU3LilnFqJOIZVtcEZHxpqdz3aQ&timestamp=1581500012&roomtype=3&logintype=2&video=200*150&companyidentify=1";
+    //NSString *urlstr = @"joinroom://?host=api.roadofcloud.net&domain=wjy&param=JxMe2Nu5uY-l_bzNjinmoaeL6LbNaatpEnJM0sSUj6In0bo9pmxZMFqVdhpay2ki8fgtSO-azH9m0x4a4uEFJpcrgbDoheztZn7cF4vFUetQvGkLFJ8gnq-59_bw0aTACrX0Ryk7uUE4BuBnoi_leAmzbr7aU3LilnFqJOIZVtcEZHxpqdz3aQ&timestamp=1581500012&roomtype=3&logintype=2&video=200*150&companyidentify=1";
+    
+    // 巡课
+    NSString *urlstr =  @"joinroom://?host=demo.roadofcloud.com&domain=xzj&param=yqf-h_RjzTMLh8pbnyIHAoyhaXjcSeaZ2LXK2b7Cadzg2o4NYECOmOxfrjFxVN42yjWfictg5s5pqIxRpJy1Gg26dxosssvbR6A-OLn12TYaWhuBL_kx7o5ZSnPRPCRIAT8qff7E-1Q&timestamp=1583057949&roomtype=3&logintype=4&video=320*240&companyidentify=1&entryUserId=67af2ebd-2321-d387-dfaa-3e08d2ca9151";
     
     NSURL *url = [NSURL URLWithString:urlstr];
     NSDictionary *dic = [[YSLiveManager shareInstance] resolveJoinRoomParamsWithUrl:url];
