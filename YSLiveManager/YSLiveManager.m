3d951ac1da117a8ca0d61983e522e074626b9b62
DennisDeng <iunion@live.cn>
2020-03-05 15:02:12 +0800

    user nil

--- YSLiveManager/YSLiveManager.m
+++ YSLiveManager/YSLiveManager.m
@@ -834,6 +834,11 @@ static YSLiveManager *liveManagerSingleton = nil;
 
 - (void)delRoomUser:(YSRoomUser *)aRoomUser showMessge:(BOOL)showMessge
 {
+    if (![aRoomUser bm_isNotEmpty])
+    {
+        return;
+    }
+    
     [self.userList removeObject:aRoomUser];
     
     NSString *roleName = nil;
