bc90ecdb7afd832d2ac85334fafde4f4570b0d3c
DennisDeng <iunion@live.cn>
2020-03-11 16:01:18 +0800

    SDK V2.4.3.0 及手册修改

--- YSLiveSDK/YSLiveSDK/YSSDKManager.m
+++ YSLiveSDK/YSLiveSDK/YSSDKManager.m
@@ -14,10 +14,10 @@
 //const unsigned char YSSDKVersionString[] = "2.0.1";
 
 /// 对应app版本
-static NSString *YSAPPVersionString = @"2.3.3";
+static NSString *YSAPPVersionString = @"2.4.3";
 
 /// SDK版本
-static NSString *YSSDKVersionString = @"2.4.0.0";
+static NSString *YSSDKVersionString = @"2.4.3.0";
 
 @interface YSSDKManager ()
 <
