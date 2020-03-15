98b0fb0cd99c8554b6e5611ee99ffa3d994c4f6b
DennisDeng <iunion@live.cn>
2020-03-15 20:18:06 +0800

    Revert "Revert "全屏白板，拖出视频丢失""
    This reverts commit 53fae0e982ab725c9b1c303ae01b21b34edb2bf5.

--- YSLive/YSSmallClass/SCMainVC.m
+++ YSLive/YSSmallClass/SCMainVC.m
@@ -2577,8 +2577,9 @@ static NSInteger studentPlayerFirst = 0; /// 播放器播放次数限制
     {
         [self.view endEditing:YES];
         
-        [self.whitebordBackgroud bm_removeAllSubviews];
-        
+        //[self.whitebordBackgroud bm_removeAllSubviews];
+        [self.whiteBordView removeFromSuperview];
+
         self.whitebordFullBackgroud.hidden = NO;
         // 加载白板
         [self.whitebordFullBackgroud addSubview:self.whiteBordView];
@@ -2587,7 +2588,9 @@ static NSInteger studentPlayerFirst = 0; /// 播放器播放次数限制
     }
     else
     {
-        [self.whitebordFullBackgroud bm_removeAllSubviews];
+        //[self.whitebordFullBackgroud bm_removeAllSubviews];
+        [self.whiteBordView removeFromSuperview];
+
         self.whitebordFullBackgroud.hidden = YES;
         
         [self.whitebordBackgroud addSubview:self.whiteBordView];
