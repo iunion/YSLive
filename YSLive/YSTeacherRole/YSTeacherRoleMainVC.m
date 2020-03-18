658cfee90352367341197532152fe60d8a91bf78
PssCloud9 <645770519@qq.com>
2020-03-18 16:13:08 +0800

    ZHIBOXBK-617 桌面共享，播放MP4，不需要举手按钮了

--- YSLive/YSTeacherRole/YSTeacherRoleMainVC.m
+++ YSLive/YSTeacherRole/YSTeacherRoleMainVC.m
@@ -5776,9 +5776,14 @@ static NSInteger playerFirst = 0; /// 播放器播放次数限制
         fullTeacherVideoView.appUseTheType = self.appUseTheType;
         [self playVideoAudioWithNewVideoView:fullTeacherVideoView];
         self.fullTeacherVideoView = fullTeacherVideoView;
-        [self.raiseHandsBtn bm_bringToFront];
         
-        self.raiseHandsBtn.frame = CGRectMake(UI_SCREEN_WIDTH-40-26, self.fullTeacherFloatView.bm_top, 40, 40);
+        if (view == self.whitebordFullBackgroud)
+        {
+            [self.raiseHandsBtn bm_bringToFront];
+            
+            self.raiseHandsBtn.frame = CGRectMake(UI_SCREEN_WIDTH-40-26, self.fullTeacherFloatView.bm_top, 40, 40);
+        }
+
     }
     
 }
