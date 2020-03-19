b057fa5ca88f7314e31552618c5ce02ea6c5a8f5
PssCloud9 <645770519@qq.com>
2020-03-20 02:46:49 +0800

    花名册上台崩溃

--- YSLive/YSTeacherRole/YSTeacherRoleMainVC.m
+++ YSLive/YSTeacherRole/YSTeacherRoleMainVC.m
@@ -2663,9 +2663,19 @@ static NSInteger playerFirst = 0; /// 播放器播放次数限制
             }
             else
             {
-//                _personListCurentPage = 0;
-                NSArray *data = [listArr subarrayWithRange:NSMakeRange(_personListCurentPage * onePageMaxUsers, onePageMaxUsers)];
-                [self.teacherListView setDataSource:data withType:SCTeacherTopBarTypePersonList userNum:studentNum];
+                //                _personListCurentPage = 0;
+                if ((_personListCurentPage + 1) * onePageMaxUsers <= (studentNum + assistantNum))
+                {
+                    NSArray *data = [listArr subarrayWithRange:NSMakeRange(_personListCurentPage * onePageMaxUsers, onePageMaxUsers)];
+                    [self.teacherListView setDataSource:data withType:SCTeacherTopBarTypePersonList userNum:studentNum];
+                }
+                else
+                {
+                    
+                    NSArray *data = [listArr subarrayWithRange:NSMakeRange(_personListCurentPage * onePageMaxUsers, listArr.count - _personListCurentPage * onePageMaxUsers)];
+                    [self.teacherListView setDataSource:data withType:SCTeacherTopBarTypePersonList userNum:studentNum];
+                }
+
             }
             
 
