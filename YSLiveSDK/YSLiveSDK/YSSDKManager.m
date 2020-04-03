a3524e3fbf7e9714706604fa68ae8f6c98d4fa54
DennisDeng <iunion@live.cn>
2020-04-03 12:35:58 +0800

    SDK V2.6.0.0

--- YSLiveSDK/YSLiveSDK/YSSDKManager.m
+++ YSLiveSDK/YSLiveSDK/YSSDKManager.m
@@ -15,10 +15,10 @@
 //const unsigned char YSSDKVersionString[] = "2.0.1";
 
 /// 对应app版本
-static NSString *YSAPPVersionString = @"2.5.1";
+static NSString *YSAPPVersionString = @"2.6.0";
 
 /// SDK版本
-static NSString *YSSDKVersionString = @"2.5.1.0";
+static NSString *YSSDKVersionString = @"2.6.0.0";
 
 @interface YSSDKManager ()
 <
