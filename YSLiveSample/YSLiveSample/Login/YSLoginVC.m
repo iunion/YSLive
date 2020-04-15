6bcc52d53fc0c8cebb7101c1c51ae0f6cabe15f6
DennisDeng <iunion@live.cn>
2020-04-15 11:47:39 +0800

    SDK V2.6.5.0

--- YSLiveSample/YSLiveSample/Login/YSLoginVC.m
+++ YSLiveSample/YSLiveSample/Login/YSLoginVC.m
@@ -124,7 +124,7 @@ static NSString *const YSLOGIN_USERDEFAULT_NICKNAME = @"ysLOGIN_USERDEFAULT_NICK
         self.joinRoomBtn.enabled = NO;
     }
 
-    NSLog(@"SDK version: %@", [YSSDKManager SDKVersion]);
+    NSLog(@"SDK version: %@", [YSSDKManager SDKDetailVersion]);
     self.ysSDKManager = [YSSDKManager sharedInstance];
     [self.ysSDKManager registerManagerDelegate:self];
     if (UI_USER_INTERFACE_IDIOM()== UIUserInterfaceIdiomPad)
