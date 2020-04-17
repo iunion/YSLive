3371b614751cc52427ca09499fa95bec25419ab1
MDI <>
2020-04-17 17:38:55 +0800

    删除的方法

--- YSLive/YSSmallClass/SCMainVC.m
+++ YSLive/YSSmallClass/SCMainVC.m
@@ -3295,10 +3295,7 @@ static NSInteger studentPlayerFirst = 0; /// 播放器播放次数限制
                 @"swfpath" : swfpath
         }
     };
-    
-    [self.liveManager.whiteBoardManager showDocumentWithFileID:fileid
-                                                    isBeginClass:self.liveManager.isBeginClass
-                                                        isPubMsg:NO];
+
     [self.liveManager.roomManager pubMsg:sShowPage msgID:sDocumentFilePage_ShowPage toID:YSRoomPubMsgTellAll data:[tDataDic1 bm_toJSON] save:YES associatedMsgID:nil associatedUserID:nil expires:0 completion:nil];
 }
 
--- YSLive/YSTeacherRole/YSTeacherRoleMainVC.m
+++ YSLive/YSTeacherRole/YSTeacherRoleMainVC.m
@@ -6495,10 +6495,7 @@ static NSInteger playerFirst = 0; /// 播放器播放次数限制
                 @"swfpath" : swfpath
         }
     };
-    
-    [self.liveManager.whiteBoardManager showDocumentWithFileID:fileid
-                                                    isBeginClass:self.liveManager.isBeginClass
-                                                        isPubMsg:NO];
+
     [self.liveManager.roomManager pubMsg:sShowPage msgID:sDocumentFilePage_ShowPage toID:YSRoomPubMsgTellAll data:[tDataDic1 bm_toJSON] save:YES associatedMsgID:nil associatedUserID:nil expires:0 completion:nil];
 }
 
