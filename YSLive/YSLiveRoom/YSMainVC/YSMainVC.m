504fbe626e50aab5d18780be9c5e26b9b5fe3e65
MDI <>
2020-04-17 16:22:31 +0800

    增加了直播中接收到个人禁言的处理，优化了全体禁言和个人禁言

--- YSLive/YSLiveRoom/YSMainVC/YSMainVC.m
+++ YSLive/YSLiveRoom/YSMainVC/YSMainVC.m
@@ -1179,6 +1179,7 @@ static const CGFloat kVideo_Height_iPad = 360.0f;
             self.chaView.chatToolView.everyoneBanChat = disablechat;
             if (disablechat)
             {
+                self.chaView.chatToolView.allDisabledChat.text = YSLocalized(@"Prompt.BanChat");
                 [self.chaView toHiddenKeyBoard];
             }
         }
@@ -1830,7 +1831,14 @@ static const CGFloat kVideo_Height_iPad = 360.0f;
 ///全体禁言
 - (void)handleSignalingToDisAbleEveryoneBanChatWithIsDisable:(BOOL)isDisable
 {
-    [self.liveManager sendSignalingToChangePropertyWithRoomUser:YSCurrentUser withKey:sUserDisablechat WithValue:@(isDisable)];
+//    [self.liveManager sendSignalingToChangePropertyWithRoomUser:YSCurrentUser withKey:sUserDisablechat WithValue:@(isDisable)];
+    
+    self.chaView.chatToolView.everyoneBanChat = isDisable;
+    if (isDisable)
+    {
+        self.chaView.chatToolView.allDisabledChat.text = YSLocalized(@"Prompt.BanChatInView");
+        [self.chaView toHiddenKeyBoard];
+    }
 }
 #pragma mark 接收消息 弹幕
 
