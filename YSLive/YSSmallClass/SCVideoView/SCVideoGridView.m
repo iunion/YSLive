d4f3b1fccd00e74edaa47dcc422e9fe83301fa4d
ningjieying <645770519@qq.com>
2020-04-26 16:04:18 +0800

    78路视频时两排

--- YSLive/YSSmallClass/SCVideoView/SCVideoGridView.m
+++ YSLive/YSSmallClass/SCVideoView/SCVideoGridView.m
@@ -143,10 +143,9 @@ static const CGFloat kVideoGridView_Gap_iPad  = 6.0f;
 
         case 7:
         case 8:
-        case 9:
         {
-            width = videoWidth*3;
-            height = videoHeight*3;
+            width = videoWidth*4;
+            height = videoHeight*2;
             
             CGFloat widthScale = (maxWidth-VIDEOGRIDVIEW_GAP*2)/width;
             CGFloat heightScale = (maxHeight-VIDEOGRIDVIEW_GAP*2)/height;
@@ -156,7 +155,7 @@ static const CGFloat kVideoGridView_Gap_iPad  = 6.0f;
             bgHeight = height*scale+VIDEOGRIDVIEW_GAP*2;
         }
             break;
-
+        case 9:
         case 10:
         case 11:
         case 12:
@@ -397,17 +396,17 @@ static const CGFloat kVideoGridView_Gap_iPad  = 6.0f;
             videoView3.bm_top = 0;
             videoView3.bm_left = width*2;
 
-            videoView4.bm_top = height;
-            videoView4.bm_left = 0;
+            videoView4.bm_top = 0;
+            videoView4.bm_left = width*3;
             
             videoView5.bm_top = height;
-            videoView5.bm_left = width;
+            videoView5.bm_left = 0;
             
             videoView6.bm_top = height;
-            videoView6.bm_left = width*2;
+            videoView6.bm_left = width;
             
-            videoView7.bm_top = height*2;
-            videoView7.bm_left = 0;
+            videoView7.bm_top = height;
+            videoView7.bm_left = width*2;
         }
             break;
 
@@ -431,20 +430,20 @@ static const CGFloat kVideoGridView_Gap_iPad  = 6.0f;
             videoView3.bm_top = 0;
             videoView3.bm_left = width*2;
 
-            videoView4.bm_top = height;
-            videoView4.bm_left = 0;
+            videoView4.bm_top = 0;
+            videoView4.bm_left = width*3;
             
             videoView5.bm_top = height;
-            videoView5.bm_left = width;
+            videoView5.bm_left = 0;
             
             videoView6.bm_top = height;
-            videoView6.bm_left = width*2;
+            videoView6.bm_left = width;
             
-            videoView7.bm_top = height*2;
-            videoView7.bm_left = 0;
+            videoView7.bm_top = height;
+            videoView7.bm_left = width*2;
             
-            videoView8.bm_top = height*2;
-            videoView8.bm_left = width;
+            videoView8.bm_top = height;
+            videoView8.bm_left = width*3;
         }
             break;
 
@@ -469,23 +468,23 @@ static const CGFloat kVideoGridView_Gap_iPad  = 6.0f;
             videoView3.bm_top = 0;
             videoView3.bm_left = width*2;
 
-            videoView4.bm_top = height;
-            videoView4.bm_left = 0;
+            videoView4.bm_top = 0;
+            videoView4.bm_left = width*3;
             
             videoView5.bm_top = height;
-            videoView5.bm_left = width;
+            videoView5.bm_left = 0;
             
             videoView6.bm_top = height;
-            videoView6.bm_left = width*2;
+            videoView6.bm_left = width;
             
-            videoView7.bm_top = height*2;
-            videoView7.bm_left = 0;
+            videoView7.bm_top = height;
+            videoView7.bm_left = width*2;
             
-            videoView8.bm_top = height*2;
-            videoView8.bm_left = width;
+            videoView8.bm_top = height;
+            videoView8.bm_left = width*3;
             
             videoView9.bm_top = height*2;
-            videoView9.bm_left = width*2;
+            videoView9.bm_left = 0;
         }
             break;
 
