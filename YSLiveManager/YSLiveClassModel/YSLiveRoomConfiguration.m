4c2e89701e592ab38a9eaf11228051d06d721fa3
ningjieying <645770519@qq.com>
2020-05-12 15:25:50 +0800

    V2.8.0.7

--- YSLiveManager/YSLiveClassModel/YSLiveRoomConfiguration.m
+++ YSLiveManager/YSLiveClassModel/YSLiveRoomConfiguration.m
@@ -58,7 +58,6 @@
         self.isHiddenKickOutStudentBtn          = [self cutOutStringWithIndex: 135];
         self.isRemindEyeCare                    = [self cutOutStringWithIndex: 141];
         self.isMultiCourseware                  = [self cutOutStringWithIndex: 150];
-        
         self.isShowUserNum                      = [self cutOutStringWithIndex: 200];
         self.isChatBeforeClass                  = [self cutOutStringWithIndex: 201];
         self.isDisablePrivateChat               = [self cutOutStringWithIndex: 202];
