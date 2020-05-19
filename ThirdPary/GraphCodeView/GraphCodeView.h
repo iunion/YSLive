7734aabf4ecd8558a5fb212dd7904db5984f8867
DennisDeng <iunion@live.cn>
2020-05-19 15:42:49 +0800

    GraphCodeView

--- ThirdPary/GraphCodeView/GraphCodeView.h
+++ ThirdPary/GraphCodeView/GraphCodeView.h
@@ -12,14 +12,15 @@
 
 @protocol GraphCodeViewDelegate <NSObject>
 
-//点击图形验证码
+// 点击图形验证码
 - (void)didTapGraphCodeView:(GraphCodeView *)graphCodeView;
 
 @end
 
 @interface GraphCodeView : UIView
 
-@property (nonatomic,strong) NSString *codeStr;//接收外部传的code
-@property (nonatomic,assign) id<GraphCodeViewDelegate> delegate;
+@property (nonatomic, assign) id <GraphCodeViewDelegate> delegate;
+
+@property (nonatomic, strong) NSString *codeStr;
 
 @end
