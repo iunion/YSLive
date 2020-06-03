bae0ed8158fd014e4214d9334eadc2f86a1cde97
DennisDeng <iunion@live.cn>
2020-06-03 11:54:07 +0800

    FLEX

---
+++ ThirdPary/FLEX/FLEX-Categories.h
@@ -0,0 +1,90 @@
+//
+//  FLEX-Categories.h
+//  FLEX
+//
+//  Created by Tanner on 3/12/20.
+//  Copyright © 2020 Flipboard. All rights reserved.
+//
+
+
+#if __has_include(<FLEX/UIBarButtonItem+FLEX.h>)
+#import <FLEX/UIBarButtonItem+FLEX.h>
+#else
+#import "UIBarButtonItem+FLEX.h"
+#endif
+#if __has_include(<FLEX/CALayer+FLEX.h>)
+#import <FLEX/CALayer+FLEX.h>
+#else
+#import "CALayer+FLEX.h"
+#endif
+#if __has_include(<FLEX/UIFont+FLEX.h>)
+#import <FLEX/UIFont+FLEX.h>
+#else
+#import "UIFont+FLEX.h"
+#endif
+#if __has_include(<FLEX/UIGestureRecognizer+Blocks.h>)
+#import <FLEX/UIGestureRecognizer+Blocks.h>
+#else
+#import "UIGestureRecognizer+Blocks.h"
+#endif
+#if __has_include(<FLEX/UIView+FLEX_Layout.h>)
+#import <FLEX/UIView+FLEX_Layout.h>
+#else
+#import "UIView+FLEX_Layout.h"
+#endif
+#if __has_include(<FLEX/UIPasteboard+FLEX.h>)
+#import <FLEX/UIPasteboard+FLEX.h>
+#else
+#import "UIPasteboard+FLEX.h"
+#endif
+#if __has_include(<FLEX/UIMenu+FLEX.h>)
+#import <FLEX/UIMenu+FLEX.h>
+#else
+#import "UIMenu+FLEX.h"
+#endif
+#if __has_include(<FLEX/UITextField+Range.h>)
+#import <FLEX/UITextField+Range.h>
+#else
+#import "UITextField+Range.h"
+#endif
+
+#if __has_include(<FLEX/NSObject+FLEX_Reflection.h>)
+#import <FLEX/NSObject+FLEX_Reflection.h>
+#else
+#import "NSObject+FLEX_Reflection.h"
+#endif
+#if __has_include(<FLEX/NSArray+FLEX.h>)
+#import <FLEX/NSArray+FLEX.h>
+#else
+#import "NSArray+FLEX.h"
+#endif
+#if __has_include(<FLEX/NSDictionary+ObjcRuntime.h>)
+#import <FLEX/NSDictionary+ObjcRuntime.h>
+#else
+#import "NSDictionary+ObjcRuntime.h"
+#endif
+#if __has_include(<FLEX/NSString+ObjcRuntime.h>)
+#import <FLEX/NSString+ObjcRuntime.h>
+#else
+#import "NSString+ObjcRuntime.h"
+#endif
+#if __has_include(<FLEX/NSString+FLEX.h>)
+#import <FLEX/NSString+FLEX.h>
+#else
+#import "NSString+FLEX.h"
+#endif
+#if __has_include(<FLEX/NSUserDefaults+FLEX.h>)
+#import <FLEX/NSUserDefaults+FLEX.h>
+#else
+#import "NSUserDefaults+FLEX.h"
+#endif
+#if __has_include(<FLEX/NSMapTable+FLEX_Subscripting.h>)
+#import <FLEX/NSMapTable+FLEX_Subscripting.h>
+#else
+#import "NSMapTable+FLEX_Subscripting.h"
+#endif
+#if __has_include(<FLEX/NSTimer+FLEX.h>)
+#import <FLEX/NSTimer+FLEX.h>
+#else
+#import "NSTimer+FLEX.h"
+#endif
