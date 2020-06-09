556e1ab9a43a7f6d2941ff6adfd91f79471c2dc0
MDI <>
2020-06-09 17:26:03 +0800

    直播：聊天输入框，表情键盘，系统消息cell，图片消息cell

--- YSLive/YSLiveRoom/YSChat/View/MessageCell/YSNewTipMessageCell.m
+++ YSLive/YSLiveRoom/YSChat/View/MessageCell/YSNewTipMessageCell.m
@@ -31,7 +31,7 @@
 - (void)setupView
 {
     self.backView = [[UIView alloc] init];
-    self.backView.backgroundColor = [UIColor bm_colorWithHexString:@"#EFF3FA"];
+    self.backView.backgroundColor = YSSkinDefineColor(@"defaultTitleColor");
     self.backView.layer.cornerRadius = 12.5;
     [self.contentView addSubview:self.backView];
     
@@ -41,7 +41,7 @@
     self.iMessageLabel.lineBreakMode = NSLineBreakByTruncatingMiddle;
     self.iMessageLabel.numberOfLines = 1;
     [self.iMessageLabel setFont:UI_FONT_12];
-    self.iMessageLabel.textColor = [UIColor bm_colorWithHexString:@"#8D9CBC"];
+    self.iMessageLabel.textColor = YSSkinDefineColor(@"placeholderColor");
     [self.backView addSubview:self.iMessageLabel];
 }
 
