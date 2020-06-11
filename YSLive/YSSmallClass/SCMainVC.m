34fe017c44647fff21fd94ff8d79bd1c5d1cbee6
ningjieying <645770519@qq.com>
2020-06-11 16:12:11 +0800

    ZHIBOXBK-989 1v1多课件：巡课应该显示 花名册/课件库/消息/退出 别的没有

--- YSLive/YSSmallClass/SCMainVC.m
+++ YSLive/YSSmallClass/SCMainVC.m
@@ -2509,6 +2509,7 @@ static NSInteger studentPlayerFirst = 0; /// 播放器播放次数限制
             case SCBottomToolBarTypeCourseware:
             {
                 //课件库
+                [self freshListViewWithSelect:isSelected];
                 if (!self.liveManager.roomConfig.isMultiCourseware)
                 {
                     self.currentMediaFileID = self.liveManager.playMediaModel.fileid;
