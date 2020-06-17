b80afcf368117adf16f6fe6a33233f60ee86999d
ningjieying <645770519@qq.com>
2020-06-17 14:55:50 +0800

    MP4播放器资源

--- YSLive/YSTools/YSMP4Player/YSMP4PlayerMaskView.m
+++ YSLive/YSTools/YSMP4Player/YSMP4PlayerMaskView.m
@@ -392,7 +392,7 @@
     
     self.loadingView.hidden = NO;
     //self.loadingImage.image = [self imagesNamedFromCustomBundle:@"icon_video_loading"];
-    self.loadingImage.image = [UIImage imageNamed:@"mp4plarer_icon_video_loading"];
+    self.loadingImage.image = YSSkinOnlineElementImage(@"online_video_loding", @"iconNor");
     if (![self.loadingImage.layer animationForKey:@"loading"]) {
         CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
         //默认是顺时针效果，若将fromValue和toValue的值互换，则为逆时针效果
@@ -566,7 +566,7 @@
         _backBtn.hidden = NO;
         _backBtn.titleLabel.font = [UIFont systemFontOfSize:17];
         //[_backBtn setImage:[self imagesNamedFromCustomBundle:@"icon_back_white"] forState:UIControlStateNormal];
-        [_backBtn setImage:[UIImage imageNamed:@"mp4plarer_icon_back_white"] forState:UIControlStateNormal];
+        [_backBtn setImage:YSSkinOnlineElementImage(@"online_video_back", @"iconNor") forState:UIControlStateNormal];
         [_backBtn addTarget:self action:@selector(backAction) forControlEvents:UIControlEventTouchUpInside];
     }
     return _backBtn;
@@ -576,9 +576,9 @@
         _playBtn = [UIButton buttonWithType:UIButtonTypeCustom];
         _playBtn.frame = CGRectMake(0, 0, 21, 21);
         //[_playBtn setImage:[self imagesNamedFromCustomBundle:@"icon_video_play"] forState:UIControlStateNormal];
-        [_playBtn setImage:[UIImage imageNamed:@"mp4plarer_icon_video_play"] forState:UIControlStateNormal];
+        [_playBtn setImage:YSSkinOnlineElementImage(@"online_video_play", @"iconNor") forState:UIControlStateNormal];
         //[_playBtn setImage:[self imagesNamedFromCustomBundle:@"icon_video_stop"] forState:UIControlStateSelected];
-        [_playBtn setImage:[UIImage imageNamed:@"mp4plarer_icon_video_stop"] forState:UIControlStateSelected];
+        [_playBtn setImage:YSSkinOnlineElementImage(@"online_video_play", @"iconSel") forState:UIControlStateSelected];
         [_playBtn addTarget:self action:@selector(startAndPause:) forControlEvents:UIControlEventTouchUpInside];
     }
     return _playBtn;
@@ -588,7 +588,7 @@
         _fullBtn = [UIButton buttonWithType:UIButtonTypeCustom];
         _fullBtn.frame = CGRectMake(0, 0, 21, 21);
         //[_fullBtn setImage:[self imagesNamedFromCustomBundle:@"icon_video_fullscreen"] forState:UIControlStateNormal];
-        [_fullBtn setImage:[UIImage imageNamed:@"mp4plarer_icon_video_fullscreen"] forState:UIControlStateNormal];
+        [_fullBtn setImage:YSSkinOnlineElementImage(@"online_video_fullscreen", @"iconNor") forState:UIControlStateNormal];
         [_fullBtn addTarget:self action:@selector(videoFullAction) forControlEvents:UIControlEventTouchUpInside];
     }
     return _fullBtn;
@@ -632,7 +632,7 @@
     if (!_videoSlider) {
         _videoSlider = [[UISlider alloc]init];
         //[_videoSlider setThumbImage:[self imagesNamedFromCustomBundle:@"icon_video_spot"] forState:UIControlStateNormal];
-        [_videoSlider setThumbImage:[UIImage imageNamed:@"mp4plarer_icon_video_spot"] forState:UIControlStateNormal];
+        [_videoSlider setThumbImage:YSSkinOnlineElementImage(@"online_video_spot", @"iconNor") forState:UIControlStateNormal];
         _videoSlider.minimumTrackTintColor = [UIColor colorWithWhite:1 alpha:0.6];
         _videoSlider.maximumTrackTintColor = [UIColor clearColor];
         // slider开始滑动事件
