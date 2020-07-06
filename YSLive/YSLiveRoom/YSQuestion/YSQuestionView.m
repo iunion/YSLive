eedfe17e908812bd0d08b014d8e7bdd498b404d2
ningjieying <645770519@qq.com>
2020-07-06 19:08:28 +0800

    提问的蒙版

--- YSLive/YSLiveRoom/YSQuestion/YSQuestionView.m
+++ YSLive/YSLiveRoom/YSQuestion/YSQuestionView.m
@@ -95,7 +95,7 @@
     [self.inputView addSubview:self.placeholdLab];
     
     self.maskView = [[UIView alloc]initWithFrame:self.bottomView.bounds];
-    self.maskView.backgroundColor = [YSSkinDefineColor(@"blackColor") changeAlpha:0.1];
+    self.maskView.backgroundColor = [YSSkinDefineColor(@"defaultTitleColor") changeAlpha:0.8];
     [self.bottomView addSubview:self.maskView];
     
     if (![YSLiveManager sharedInstance].isClassBegin)
