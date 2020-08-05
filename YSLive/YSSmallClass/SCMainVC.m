31f7a0dbbf42aa93a565f2884055dd663d39b8c3
DennisDeng <iunion@live.cn>
2020-08-05 11:30:08 +0800

    刷新奖杯数

--- YSLive/YSSmallClass/SCMainVC.m
+++ YSLive/YSSmallClass/SCMainVC.m
@@ -143,7 +143,6 @@ static NSInteger studentPlayerFirst = 0; /// 播放器播放次数限制
     BOOL giftMp3Playing;
 }
 
-
 /// 奖杯数请求
 @property (nonatomic, strong) NSURLSessionDataTask *giftCountTask;
 
@@ -3113,8 +3112,13 @@ static NSInteger studentPlayerFirst = 0; /// 播放器播放次数限制
         YSRoomUser *fromUser = [self.liveManager getRoomUserWithId:fromeUserId];
         if (fromUser.role != YSUserType_Student && videoViewArr.count)
         {
+            NSUInteger giftNumber = [properties bm_uintForKey:sYSUserGiftNumber];
+            for (SCVideoView *videoView in videoViewArr)
+            {
+                videoView.giftNumber = giftNumber;
+            }
+
             SCVideoView * videoView = videoViewArr[0];
-            videoView.giftNumber =  [properties bm_uintForKey:sYSUserGiftNumber];
             [self showGiftAnimationWithVideoView:videoView];
         }
     }
