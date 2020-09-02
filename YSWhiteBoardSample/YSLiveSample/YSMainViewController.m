799dbff53ec34c73cdbc30d6bf4905308dde6ee1
MDI <>
2020-09-02 17:21:57 +0800

    SDKSample添加画笔工具，换肤文件

--- YSWhiteBoardSample/YSLiveSample/YSMainViewController.m
+++ YSWhiteBoardSample/YSLiveSample/YSMainViewController.m
@@ -7,32 +7,155 @@
 //
 
 #import "YSMainViewController.h"
+#import "SCBrushToolView.h"
+#import "SCDrawBoardView.h"
+
 
 @interface YSMainViewController ()
+<
+    SCBrushToolViewDelegate,
+    SCDrawBoardViewDelegate
+>
+
+
+@property (nonatomic, weak) YSWhiteBoardSDKManager *whiteBoardSDKManager;
+/// 固定UserId
+@property (nonatomic, strong) NSString *userId;
+
+/// 主白板
+@property (nonatomic, strong) UIView *mainWhiteBoardView;
+
+/// 左侧工具栏
+@property (nonatomic, strong) SCBrushToolView *brushToolView;
+/// 画笔工具按钮（控制工具条的展开收起）
+@property (nonatomic, strong) UIButton *brushToolOpenBtn;
+/// 画笔选择 颜色 大小 形状
+@property (nonatomic, strong) SCDrawBoardView *drawBoardView;
 
 @end
 
 @implementation YSMainViewController
+
+- (instancetype)initWithwhiteBordView:(UIView *)whiteBordView userId:(nullable NSString *)userId
+{
+    self = [self init];
+    if (self)
+    {
+        self.userId = userId;
+        self.mainWhiteBoardView = whiteBordView;
+        self.whiteBoardSDKManager = [YSWhiteBoardSDKManager sharedInstance];
+    }
+    return self;
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
     self.view.backgroundColor = UIColor.whiteColor;
+        
+    // 白板背景
+//    UIView *whitebordBackgroud = [[UIView alloc] initWithFrame:CGRectMake(50, 50, self.view.frame.size.height - 100, self.view.frame.size.width - 100)];
+//    [self.view addSubview:whitebordBackgroud];
+//    whitebordBackgroud.backgroundColor = UIColor.yellowColor;
     
-    self.navigationItem.title = @"白板页";
+    [self.view addSubview:self.mainWhiteBoardView];
+    self.mainWhiteBoardView.frame = CGRectMake(0, 0, self.view.bm_height, self.view.bm_width);
     
-    
+    [self setupBrushToolView];
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark UI 工具栏
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+/// 设置左侧工具栏
+- (void)setupBrushToolView
+{
+    self.brushToolView = [[SCBrushToolView alloc] initWithTeacher:NO];
+    [self.view addSubview:self.brushToolView];
+
+    CGFloat laftGap = 10;
+    if (BMIS_IPHONEXANDP)
+    {
+        laftGap = BMUI_HOME_INDICATOR_HEIGHT;
+    }
+    self.brushToolView.bm_left = laftGap;
+    self.brushToolView.bm_centerY = self.view.bm_centerY;
+    self.brushToolView.delegate = self;
+//    self.brushToolView.hidden = YES;
+    
+    UIButton *brushToolOpenBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [brushToolOpenBtn addTarget:self action:@selector(brushToolOpenBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [brushToolOpenBtn setBackgroundImage:YSSkinElementImage(@"brushTool_open", @"iconNor") forState:UIControlStateNormal];
+    [brushToolOpenBtn setBackgroundImage:YSSkinElementImage(@"brushTool_open", @"iconSel") forState:UIControlStateSelected];
+    brushToolOpenBtn.frame = CGRectMake(0, 0, 25, 37);
+    brushToolOpenBtn.bm_centerY = self.brushToolView.bm_centerY;
+    brushToolOpenBtn.bm_left = self.brushToolView.bm_right;
+    self.brushToolOpenBtn = brushToolOpenBtn;
+//    self.brushToolOpenBtn.hidden = YES;
+    [self.view addSubview:brushToolOpenBtn];
 }
-*/
+
+#pragma mark 画笔工具展开收起
+- (void)brushToolOpenBtnClick:(UIButton *)btn
+{
+//    if (self.liveManager.isBeginClass)
+       {
+           btn.selected = !btn.selected;
+           CGFloat leftGap = 10;
+           if (BMIS_IPHONEXANDP)
+           {
+               leftGap = BMUI_HOME_INDICATOR_HEIGHT;
+           }
+           CGFloat tempWidth = [UIDevice bm_isiPad] ? 50.0f : 36.0f;
+           if (btn.selected)
+           {
+//               self.drawBoardView.hidden = YES;
+               [UIView animateWithDuration:0.3 animations:^{
+                   self.brushToolView.bm_left = -tempWidth;
+                   self.brushToolOpenBtn.bm_left = leftGap;
+               }];
+           }
+           else
+           {
+               [UIView animateWithDuration:0.3 animations:^{
+                   self.brushToolView.bm_left = leftGap;
+                   self.brushToolOpenBtn.bm_left = self.brushToolView.bm_right;
+               }];
+           }
+       }
+}
+
+#pragma mark SCBrushToolViewDelegate
+
+- (void)brushToolViewType:(YSBrushToolType)toolViewBtnType withToolBtn:(nonnull UIButton *)toolBtn
+{
+    [self.whiteBoardSDKManager brushSDKToolsDidSelect:toolViewBtnType];
+
+    if (self.drawBoardView)
+    {
+        [self.drawBoardView removeFromSuperview];
+    }
+    
+    self.drawBoardView = [[SCDrawBoardView alloc] init];
+    self.drawBoardView.delegate = self;
+    self.drawBoardView.brushToolType = toolViewBtnType;
+    [self.view addSubview:self.drawBoardView];
+    
+    BMWeakSelf
+    [self.drawBoardView.backgroundView  bmmas_makeConstraints:^(BMMASConstraintMaker *make) {
+        make.left.bmmas_equalTo(weakSelf.brushToolOpenBtn.bmmas_right).bmmas_offset(10);
+        make.centerY.bmmas_equalTo(weakSelf.brushToolOpenBtn.bmmas_centerY);
+    }];
+}
+
+#pragma mark - 需要传递给白板的数据
+#pragma mark SCDrawBoardViewDelegate
+
+- (void)brushSelectorViewDidSelectDrawType:(YSDrawType)drawType color:(NSString *)hexColor widthProgress:(float)progress
+{
+    [self.whiteBoardSDKManager didSDKSelectDrawType:drawType color:hexColor widthProgress:progress];
+}
+
 
 @end
---
+++ YSWhiteBoardSample/YSLiveSample/YSSkinManager/YSLiveSkinManager.m
@@ -0,0 +1,162 @@
+//
+//  YSSkinManager.m
+//  YSAll
+//
+//  Created by 马迪 on 2020/5/25.
+//  Copyright © 2020 YS. All rights reserved.
+//
+
+#import "YSLiveSkinManager.h"
+
+static YSLiveSkinManager *skinManager = nil;
+
+#define YSSkinBundleName    (self.classOrOnline == YSSkinClassOrOnline_class)?@"YSSkinRsource.bundle": @"YSOnlineSchool.bundle"
+#define YSSkinBundle        [NSBundle bundleWithPath:[[NSBundle bm_mainResourcePath] stringByAppendingPathComponent:YSSkinBundleName]]
+
+@interface YSLiveSkinManager ()
+
+@property (nonatomic, assign) YSSkinType lastSkinType;
+
+@property (nonatomic, assign) YSSkinClassOrOnline lastClassOrOnline;
+
+@property (nonatomic, strong) NSDictionary *plictDict;
+
+@end
+
+@implementation YSLiveSkinManager
+
++ (instancetype)shareInstance
+{
+    @synchronized(self)
+    {
+        if (!skinManager)
+        {
+            skinManager = [[YSLiveSkinManager alloc] init];
+            
+            skinManager.skinType = YSSkinType_black;
+        }
+    }
+    return skinManager;
+}
+
+/// 获取plist文件中的数据
+- (NSDictionary *)getPliatDictionaryWithType:(YSSkinClassOrOnline)classOrOnline
+{
+    
+    if (self.lastClassOrOnline != self.classOrOnline || self.lastSkinType != self.skinType || ![self.plictDict bm_isNotEmpty])
+    {
+        NSString *path = nil;
+        
+        if (classOrOnline == YSSkinClassOrOnline_class)
+        {
+            //        if (self.skinType == YSSkinType_original)
+            //        {//原始颜色背景 （蓝）
+            //            path = [YSSkinBundle pathForResource:@"OriginalColor" ofType:@"plist"];
+            //        }
+            //        else if (self.skinType == YSSkinType_black)
+                    {//黑色背景
+                        path = [YSSkinBundle pathForResource:@"BlackColor" ofType:@"plist"];
+                    }
+        }
+        else
+        {
+            //黑色背景
+            path = [YSSkinBundle pathForResource:@"onlineBlackColor" ofType:@"plist"];
+        }
+            
+        NSString * sss = YSSkinBundleName;
+        
+        self.plictDict = [NSDictionary dictionaryWithContentsOfFile:path];
+    }
+    
+    self.lastSkinType = self.skinType;
+    self.lastClassOrOnline = self.classOrOnline;
+    
+    return self.plictDict;
+}
+
+///默认颜色
+- (UIColor *)getDefaultColorWithType:(YSSkinClassOrOnline)classOrOnline WithKey:(NSString *)key
+{
+    self.classOrOnline = classOrOnline;
+    NSDictionary *colorDict = [[self getPliatDictionaryWithType:classOrOnline] bm_dictionaryForKey:@"CommonColor"];
+    NSString *colorStr = [colorDict bm_stringForKey:key];
+
+    UIColor *color = [UIColor bm_colorWithHexString:colorStr];
+    
+    return color;
+}
+
+//默认图片
+- (UIImage *)getDefaultImageWithType:(YSSkinClassOrOnline)classOrOnline WithKey:(NSString *)key
+{
+    self.classOrOnline = classOrOnline;
+    NSDictionary *imageDict = [[self getPliatDictionaryWithType:classOrOnline] bm_dictionaryForKey:@"CommonImage"];
+    NSString *imageName = [imageDict bm_stringForKey:key];
+    
+    UIImage *image = [self getBundleImageWithType:classOrOnline WithImageName:imageName];
+    
+    return image;
+}
+
+///控件颜色
+- (UIColor *)getElementColorWithType:(YSSkinClassOrOnline)classOrOnline WithName:(NSString *)name andKey:(NSString *)key
+{
+    self.classOrOnline = classOrOnline;
+    NSDictionary *elementDict = [[self getPliatDictionaryWithType:classOrOnline] bm_dictionaryForKey:name];
+    
+    if ([elementDict bm_isNotEmpty])
+    {
+        NSString *colorStr = [elementDict bm_stringForKey:key];
+        
+        UIColor *color = [UIColor bm_colorWithHexString:colorStr];
+        return color;
+    }
+    
+    return nil;
+}
+
+///控件图片
+- (UIImage *)getElementImageWithType:(YSSkinClassOrOnline)classOrOnline WithName:(NSString *)name andKey:(NSString *)key
+{
+    self.classOrOnline = classOrOnline;
+    
+    NSDictionary *elementDict = [[self getPliatDictionaryWithType:classOrOnline] bm_dictionaryForKey:name];
+    
+    if ([elementDict bm_isNotEmpty])
+    {
+        NSString *imageName = [elementDict bm_stringForKey:key];
+        
+        UIImage *image =  [self getBundleImageWithType:classOrOnline  WithImageName:imageName];
+        return image;
+    }
+    
+    return nil;
+}
+
+- (UIImage *)getBundleImageWithType:(YSSkinClassOrOnline)classOrOnline WithImageName:(NSString *)imageName
+{
+    NSString *imageFolder = nil;
+    
+    if (classOrOnline == YSSkinClassOrOnline_class)
+    {
+        //    if (self.skinType == YSSkinType_original)
+        //    {//原始颜色背景 （蓝）
+        //        imageFolder = @"YSSkinOriginal";
+        //    }
+        //    else if (self.skinType == YSSkinType_black)
+            {//黑色背景
+                imageFolder = @"YSSkinBlack";
+            }
+    }
+    else
+    {
+        //黑色背景
+        imageFolder = @"onLineSkinBlack";
+    }
+        
+    UIImage *image = [YSSkinBundle bm_imageWithAssetsName:imageFolder imageName:imageName];
+    return image;
+}
+
+@end
