1f6a0c22ecbb55cc12cf786ae564396ad7116658
ningjieying <645770519@qq.com>
2020-09-22 14:04:46 +0800

    zybb

---
+++ zybb/zybb/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  zybb
+//
+//  Created by fzxm on 2020/9/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ zybb/zybb/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  zybb
+//
+//  Created by fzxm on 2020/9/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
