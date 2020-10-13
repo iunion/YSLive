1cf5397c9797a71c12373cbe9483d2a10672510c
ningjieying <645770519@qq.com>
2020-10-13 14:40:31 +0800

    倒计时按钮未完成

---
+++ BMKit/BMKit/BMTools/BMCountDownButton/BMCountDownButton.m
@@ -0,0 +1,19 @@
+//
+//  BMCountDownButton.m
+//  BMKit
+//
+//  Created by njy on 2020/10/13.
+//  Copyright © 2020 DennisDeng. All rights reserved.
+//
+
+#import "BMCountDownButton.h"
+
+@interface BMCountDownButton ()
+
+@end
+
+@implementation BMCountDownButton
+
+
+
+@end
