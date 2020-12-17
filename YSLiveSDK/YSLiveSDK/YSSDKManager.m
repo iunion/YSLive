cfe97d7680c10b5d99206c718497a525c2d2684b
DennisDeng <iunion@live.cn>
2020-12-17 17:51:46 +0800

    UI SDK V3.4.1.0

--- YSLiveSDK/YSLiveSDK/YSSDKManager.m
+++ YSLiveSDK/YSLiveSDK/YSSDKManager.m
@@ -15,10 +15,10 @@
 //const unsigned char YSSDKVersionString[] = "2.0.1";
 
 /// 对应app版本
-static NSString *YSAPPVersionString = @"3.4.0";
+static NSString *YSAPPVersionString = @"3.4.1";
 
 /// SDK版本
-static NSString *YSSDKVersionString = @"3.4.0.0";
+static NSString *YSSDKVersionString = @"3.4.1.0";
 
 @interface YSSDKManager ()
 <
