5d23906092b1c8b2b134c7c70465460bbc5092bf
ningjieying <645770519@qq.com>
2020-12-30 15:15:20 +0800

    新liveSDK

---
+++ YSLiveSDK/FFF/YSLiveSDK/YSLiveSDK/YSLiveSDK.h
@@ -0,0 +1,18 @@
+//
+//  YSLiveSDK.h
+//  YSLiveSDK
+//
+//  Created by fzxm on 2020/12/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YSLiveSDK.
+FOUNDATION_EXPORT double YSLiveSDKVersionNumber;
+
+//! Project version string for YSLiveSDK.
+FOUNDATION_EXPORT const unsigned char YSLiveSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YSLiveSDK/PublicHeader.h>
+
+
