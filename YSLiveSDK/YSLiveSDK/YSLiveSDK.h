07a026b3865914f85d970b38325e909402ddea71
ningjieying <645770519@qq.com>
2020-12-31 14:17:21 +0800

    YSLiveSDK

--- YSLiveSDK/YSLiveSDK/YSLiveSDK.h
+++ YSLiveSDK/YSLiveSDK/YSLiveSDK.h
@@ -2,19 +2,19 @@
 //  YSLiveSDK.h
 //  YSLiveSDK
 //
-//  Created by jiang deng on 2019/11/27.
-//  Copyright © 2019 YS. All rights reserved.
+//  Created by fzxm on 2020/12/29.
 //
 
 #import <Foundation/Foundation.h>
 
 //! Project version number for YSLiveSDK.
 //FOUNDATION_EXPORT double YSLiveSDKVersionNumber;
+//
+////! Project version string for YSLiveSDK.
+//FOUNDATION_EXPORT const unsigned char YSLiveSDKVersionString[];
+//
+//// In this header, you should import all the public headers of your framework using statements like #import <YSLiveSDK/PublicHeader.h>
 
-//! Project version string for YSLiveSDK.
-//FOUNDATION_EXPORT const unsigned char YSSDKVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <YSLiveSDK/PublicHeader.h>
 
 #import <BMKit/BMKit.h>
 
