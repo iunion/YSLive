36e02af18d1f3cc9eff3d4bc41c26ec94a70ecab
DennisDeng <iunion@live.cn>
2021-02-05 16:36:37 +0800

    BMKeystoneCorrectionView

---
+++ YSLive/YSTools/BMKeystoneCorrectionView/BMKeystoneCorrectionView.h
@@ -0,0 +1,17 @@
+//
+//  BMKeystoneCorrectionView.h
+//  YSLive
+//
+//  Created by jiang deng on 2021/2/5.
+//  Copyright © 2021 YS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BMKeystoneCorrectionView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YSLive/YSTools/BMKeystoneCorrectionView/BMKeystoneCorrectionView.m
@@ -0,0 +1,21 @@
+//
+//  BMKeystoneCorrectionView.m
+//  YSLive
+//
+//  Created by jiang deng on 2021/2/5.
+//  Copyright © 2021 YS. All rights reserved.
+//
+
+#import "BMKeystoneCorrectionView.h"
+
+@implementation BMKeystoneCorrectionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
