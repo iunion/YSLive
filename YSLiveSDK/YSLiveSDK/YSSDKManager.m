4f7978701ea32dd194d068e9a4b92cc338b6f7c8
ningjieying <645770519@qq.com>
2021-04-15 11:59:39 +0800

    YSLiveSDK 3.5.8.2

--- YSLiveSDK/YSLiveSDK/YSSDKManager.m
+++ YSLiveSDK/YSLiveSDK/YSSDKManager.m
@@ -16,10 +16,10 @@
 //const unsigned char YSSDKVersionString[] = "2.0.1";
 
 /// 对应app版本
-static NSString *YSAPPVersionString = @"3.5.7";
+static NSString *YSAPPVersionString = @"3.5.8";
 
 /// SDK版本
-static NSString *YSSDKVersionString = @"3.5.7.0";
+static NSString *YSSDKVersionString = @"3.5.8.2";
 
 @interface YSSDKManager ()
 <
